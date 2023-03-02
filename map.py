55ad0b4a27e29d1d25281edfe3f703db84bd5b52
Andm <ktenshi@Ands-Mac.lan>
2023-03-01 22:58:39 -0600

    Adding map and collision

--- map.py
+++ map.py
@@ -18,14 +18,15 @@ mini_map = [
 class Map:
     def __init__(self, game):
         self.game = game
-        self.mini_map = mini_map
+        self.mini_map = mini_map   # declaring and initializing the mini/world map
         self.world_map = {}
         self.get_map()
+
     def get_map(self):
         for j, row in enumerate(self.mini_map):
             for i, value in enumerate(row):
                 if value:
-                    self.world_map[(i, j)] = value
+                    self.world_map[(i, j)] = value   # Here is where we generate the map
 
     def draw(self):
         [pg.draw.rect(self.game.screen, 'darkgray', (pos[0] * 100, pos[1] * 100, 100, 100), 2)
